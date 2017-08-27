1f871306fc166ea37986618c73c5e5626ba45319
fnscoder <felipensouza@live.com>
2017-08-27 03:46:32 -0300

    config

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,6 +21,8 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+BIO = 'Web developer'
+
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
